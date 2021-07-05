b8cb5b88129e6652032bc2e95e353b9a23bf6b89
minsoo kim <zlemzlem5656@naver.com>
2021-07-05 20:40:30 +0900

    [추가] 상세 페이지

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
-    </>
+
+      <Switch>
+        <Route exact path="/detail">
+          <Detail />
+        </Route>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
