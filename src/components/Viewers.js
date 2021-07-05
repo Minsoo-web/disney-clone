b436b831fcc2fa3ffdb9c37fd49717a33f8ef7fc
minsoo kim <zlemzlem5656@naver.com>
2021-07-05 18:39:34 +0900

    [추가] Viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,7 +1,53 @@
 import React from "react";
+import styled from "styled-components";
 
 function Viewers() {
-  return <div>Viewers</div>;
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="viewers" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="viewers" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="viewers" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="viewers" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="viewers" />
+      </Wrap>
+    </Container>
+  );
 }
 
 export default Viewers;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  grid-gap: 25px;
+  margin-top: 30px;
+  padding: 30px 0 26px;
+  cursor: pointer;
+`;
+
+const Wrap = styled.div`
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 40px 58px -16px, rgb(0 0 0 / 73%) 0px 30px 22px -10px;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
