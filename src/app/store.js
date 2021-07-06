1b01ba7113e82403499abefc666b6432f40e64ae
minsoo kim <zlemzlem5656@naver.com>
2021-07-06 21:35:08 +0900

    [추가] grab movie from firebase db

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import movieReducre from "../features/movie/movieSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    movie: movieReducre
+  }
 });
